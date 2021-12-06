b57cdf5f227ecf526607ae7b68ca8e79841eee16
DrozdetskySergey <master@2867073.ru>
2021-12-06 18:59:42 +0700

    JQuery TODO list. Done

---
+++ JQuery_TODO_List/script.js
@@ -0,0 +1,96 @@
+$(function () {
+    var inputText = $(".input");
+    var addButton = $(".add_button");
+    var list = $(".list");
+
+    function editInputTextPlaceholder() {
+        var listItemCount = $(".list .list_item").length;
+
+        inputText.attr("placeholder", String(listItemCount + 1));
+    }
+
+    function addListItem() {
+        var text = inputText.val().trim();
+        var listItem = $("<li>");
+
+        function setViewMode() {
+            listItem.html("<p class='list_item_text'></p>"
+                + "<button class='edit_button'>Редактировать</button> <button class='delete_button'>Удалить</button>");
+
+            listItem.find(".delete_button").click(function () {
+                listItem.remove();
+
+                editInputTextPlaceholder();
+            });
+
+            listItem.find(".edit_button").click(function () {
+                setEditMode();
+            });
+
+            listItem.find(".list_item_text").text(text);
+        }
+
+        function setEditMode() {
+            listItem.html("<input class='list_item_edit'>"
+                + "<button class='save_button'>Сохранить</button> <button class='cancel_button'>Отмена</button>");
+
+            var editInput = listItem.find(".list_item_edit");
+
+            function editListItem() {
+                var editText = editInput.val().trim();
+
+                if (!editText) {
+                    alert("Нельзя оставлять пустую запись.");
+
+                    return;
+                }
+
+                text = editText;
+
+                setViewMode();
+            }
+
+            listItem.find(".save_button").click(editListItem);
+
+            listItem.find(".cancel_button").click(function () {
+                setViewMode();
+            });
+
+            editInput.on("keydown", function (event) {
+                if (event.code === "Enter") {
+                    event.preventDefault();
+
+                    editListItem();
+                }
+            });
+
+            editInput.focus();
+            editInput.val(text);
+        }
+
+        if (!text) {
+            alert("Нельзя добавлять пустую запись.");
+
+            return;
+        }
+
+        setViewMode();
+
+        listItem.addClass("list_item");
+        list.append(listItem);
+        inputText.focus();
+        inputText.val("");
+
+        editInputTextPlaceholder();
+    }
+
+    addButton.click(addListItem);
+
+    inputText.on("keydown", function (event) {
+        if (event.code === "Enter") {
+            event.preventDefault();
+
+            addListItem();
+        }
+    });
+})();
